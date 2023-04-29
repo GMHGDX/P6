9f02559b1bdddd72b85ce61128044791572da12e
GabrielleHieken <gabriellehieken@gmail.com>
2023-04-28 20:00:24 -0500

    steps for completion

--- oss.h
+++ oss.h
@@ -1,1 +1,44 @@
-//hogwla
+//What needs to be done:
+//FIFO (clock) paage replacement algo, circular queue
+//allocate shared memory for data structures/page table
+//create fixed size arrays for page table assuming processes require less than 32k mem and each page is 1k
+//make sure process don't access memory outside the page limit 
+//have child runs in loop constantly until terminated
+//oss check if there is a page fault and block process if there is (increment by 100 nano if no fault)
+//Each request for disk read/write takes 14 ms to fulfill
+//Child request random meory space 1 - 32 then multipy it by 1024 and add a random offset between 0 - 1023 to get actual memory addree
+//Once child request memory space randomly choose if it will have read or write(with more incline to read) then increment the clock
+//check if process should terminate at random time and return all memory to oss
+
+//try to documment the following: 
+// Number of memory accesses per second
+// Number of page faults per memory access
+// Average memory access speed
+//terminate after 100 processes/2 real second
+
+//What this program should have:
+//system memory has 256k
+//frame table
+//page table
+//18 processes running at a time (use define macro as a hard limit)
+
+// Print Example:
+// Master: P2 requesting read of address 25237 at time xxx:xxx
+// Master: Address 25237 in frame 13, giving data to P2 at time xxx:xxx
+// Master: P5 requesting write of address 12345 at time xxx:xxx
+// Master: Address 12345 in frame 203, writing data to frame at time xxx:xxx
+// Master: P2 requesting write of address 03456 at time xxx:xxx
+// Master: Address 12345 is not in a frame, pagefault
+// Master: Clearing frame 107 and swapping in p2 page 3
+// Master: Dirty bit of frame 107 set, adding additional time to the clock
+// Master: Indicating to P2 that write has happened to address 03456
+
+// Current memory layout at time xxx:xxx is:
+        // Occupied DirtyBit HeadOfFIFO
+// Frame 0: No      0
+// Frame 1: Yes     1
+// Frame 2: Yes     0        *
+// Frame 3: Yes     1
+
+// where Occupied indicates if we have a page in that frame, the * indicates that the next page will be replaced there,
+// and the dirty bit indicates if the frame has been written to.
