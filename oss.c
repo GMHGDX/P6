15fe87f7990d0cd8935bce7a29e0928c31d7fefd
GabrielleHieken <gabriellehieken@gmail.com>
2023-05-08 22:17:49 -0500

    printing frame table

--- oss.c
+++ oss.c
@@ -247,11 +247,13 @@ int main(int argc, char *argv[]){
                             frameTable[headpointer][1] = 0; //dirty bit
                             frameTable[headpointer][0] = 1; //unoccupied
 
+                            strcpy(buf.strData, headpointer);
+                            printf("OSS - The buf.str data: %s\n", buf.strData);
+                            
                             printf("OSS: Clearing frame %i and swapping in PIDs %d page %i\n", headpointer ,childpid, page);
                             //Send message back to user process
                             printf("OSS: Indicating to %d that write has happened to address %i\n", childpid, memoryAddress);
-                            strcpy(buf.strData, headpointer);
-                            printf("OSS - The buf.str data: %s\n", buf.strData);
+                            
                             buf.intData = getpid();
                             buf.mtype = childpid;
                             if(msgsnd(msqid, &buf, sizeof(msgbuffer), 0 == -1)){ perror("msgsnd from child to parent failed\n"); exit(1); }
