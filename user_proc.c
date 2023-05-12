eae748830b882af3ee2dede0765f82dc2a1e414b
GabrielleHieken <gabriellehieken@gmail.com>
2023-05-11 20:52:09 -0500

    print frame table

--- user_proc.c
+++ user_proc.c
@@ -40,94 +40,85 @@ int main(int argc, char *argv[]){
 
     //read system time from memory
     struct Table readFromMemWorker;
-    readFromMemWorker = *shm_ptr;
-    //Systime = readFromMemWorker.currentTime;  
-
-    //request memory to random page
-    page = randomNumberGenerator(32); //max pages a process can request is 32
-    randomOffset = randomNumberGenerator(1023); //max offset is 1023
-    memoryAddress = (page * 1024) + randomOffset;
-
-    if(memoryAddress > 32000){ memoryAddress = 32000; } //if memory address exceeds 32000, keep it at 32000
-
-    //Process chooses if it will read or write (more inclined to read)
-    readWrite = randomNumberGenerator(100);
-    if(readWrite < 70){
-        readWrite = 1; //requesting read
-
-    } else{
-        readWrite = 2; //requesting write
-    }
     
-    //Convert integer to string
-    char memory[50];
-    char permission[50];
-    char pageNum[50];
-    snprintf(memory, sizeof(memory), "%i", memoryAddress);
-    snprintf(permission, sizeof(permission), "%i", readWrite);
-    snprintf(pageNum, sizeof(pageNum), "%i", page);
-
-    //add seconds and nanoseconds together with a space in between to send as one message
-    char *together;
-    together = malloc(strlen(memory) + strlen(permission) + strlen(pageNum) + 1 + 1 + 1);
-    strcpy(together, memory);
-    strcat(together, " ");
-    strcat(together, permission);
-    strcat(together, " ");
-    strcat(together, pageNum);
-
-    //send our string to message queue
-    strcpy(buf.strData, together); //copy our new string into string data buffer
-    buf.intData = getpid();
-    buf.mtype = (long)getppid();
-    if(msgsnd(msqid, &buf, sizeof(msgbuffer), 0 == -1)){ perror("msgsnd from child to parent failed\n"); exit(1); }
-
-    if(readWrite == 2){  //recieve frame table from OSS
-    if (msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) 
-        { perror("2 failed to receive message from parent\n"); exit(1); }
-        
-        frameNumber = atoi(buf.strData);
-        //write new page table to memory
-        struct Table writeToMemWorker;
-        //Print contents of page table
-        printf("--Page Table--\n");
-        for(i = 0; i < 32; i++){
-            printf("Page%i\t", i+1);
-            writeToMemWorker.pageTable[i] = readFromMemWorker.pageTable[i];
-            writeToMemWorker.pageTable[page-1] = frameNumber;
-            printf("%i\t", writeToMemWorker.pageTable[i]);
-            printf("\n");
-        }
-        writeToMemWorker.currentTime = readFromMemWorker.currentTime;
-        *shm_ptr = writeToMemWorker;
-    }
-    //Recieve message back from oss on when child should terminate
-    while(1){
-        if (msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) 
-        {
-            perror("2 failed to receive message from parent\n"); 
-            exit(1);
-        }
-        checkResponse = atoi(buf.strData);
-
-        if(checkResponse == 1){
-            break;
-        }
-    }
-    int loopAmount = 0;
     int loopAgain;
-    while(loopAmount < 1001){ //Check to terminate after it loops 1000 times, randomly terminates if 1000 passes
-        loopAmount++;
-        if(loopAmount == 1001){
-            loopAgain = randomNumberGenerator(2);
-            if(loopAgain == 1){
-                loopAmount = 0;
-                printf("Worker - Child Decide to loop again!\n");
+    while(1){ //Check to terminate after it loops 1000 times, randomly terminate 
+        loopAgain = randomNumberGenerator(2);
+        if(loopAgain == 1){
+            printf("Worker - Child %d decided to request another page!\n", getpid());
+            readFromMemWorker = *shm_ptr; 
+
+            //request memory to random page
+            page = randomNumberGenerator(32); //max pages a process can request is 32
+            randomOffset = randomNumberGenerator(1023); //max offset is 1023
+            memoryAddress = (page * 1024) + randomOffset;
+
+            if(memoryAddress > 32000){ memoryAddress = 32000; } //if memory address exceeds 32000, keep it at 32000
+
+            //Process chooses if it will read or write (more inclined to read)
+            readWrite = randomNumberGenerator(100);
+            if(readWrite < 70){
+                readWrite = 1; //requesting read
+
+            } else{
+                readWrite = 2; //requesting write
             }
-            if(loopAgain == 2){
-                printf("Worker - Child is terminating!\n");
-                break;
+            
+            //Convert integer to string
+            char memory[50];
+            char permission[50];
+            char pageNum[50];
+            snprintf(memory, sizeof(memory), "%i", memoryAddress);
+            snprintf(permission, sizeof(permission), "%i", readWrite);
+            snprintf(pageNum, sizeof(pageNum), "%i", page);
+
+            //add seconds and nanoseconds together with a space in between to send as one message
+            char *together;
+            together = malloc(strlen(memory) + strlen(permission) + strlen(pageNum) + 1 + 1 + 1);
+            strcpy(together, memory);
+            strcat(together, " ");
+            strcat(together, permission);
+            strcat(together, " ");
+            strcat(together, pageNum);
+
+            //send our string to message queue
+            strcpy(buf.strData, together); //copy our new string into string data buffer
+            buf.intData = getpid();
+            buf.mtype = (long)getppid();
+            if(msgsnd(msqid, &buf, sizeof(msgbuffer), 0 == -1)){ perror("msgsnd from child to parent failed\n"); exit(1); }
+
+            if(readWrite == 2){  //recieve frame table from OSS
+            if (msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) 
+                { perror("2 failed to receive message from parent\n"); exit(1); }
+                
+                frameNumber = atoi(buf.strData);
+                //write new page table to memory
+                struct Table writeToMemWorker;
+                //Print contents of page table
+                printf("--Page Table--\n");
+                for(i = 0; i < 32; i++){
+                    printf("Page%i\t", i+1);
+                    writeToMemWorker.pageTable[i] = readFromMemWorker.pageTable[i];
+                    writeToMemWorker.pageTable[page-1] = frameNumber;
+                    printf("%i\t", writeToMemWorker.pageTable[i]);
+                    printf("\n");
+                }
+                writeToMemWorker.currentTime = readFromMemWorker.currentTime;
+                *shm_ptr = writeToMemWorker;
             }
+            //Recieve message back from oss on when child should terminate
+            while(1){
+                if (msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) { perror("2 failed to receive message from parent\n"); exit(1); }
+                checkResponse = atoi(buf.strData);
+                
+                if(checkResponse == 1){
+                    break;
+                }
+            }
+        }
+        if(loopAgain == 2){
+            printf("Worker - Child %d is terminating!\n", getpid());
+            break;
         }
     }
     return 0;
