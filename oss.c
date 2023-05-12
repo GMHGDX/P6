0d0e7b3a9f6af01439f00d80a0d22c4d3588849b
GabrielleHieken <gabriellehieken@gmail.com>
2023-05-11 21:38:11 -0500

    print frame table

--- oss.c
+++ oss.c
@@ -154,14 +154,9 @@ int main(int argc, char *argv[]){
         }
         msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0); // IPC_NOWAIT receive a message from user_proc, but only one for our PID, dont wait for a message
          dead = atoi(buf.strData);
-        //if(buf.strData == "404"){
-            //check if a process is dead (skull emoji)
-            //msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0);
-            //dead = atoi(buf.strData);
-            if(dead == 404){
-                processRunning--;
-            }
-        //}
+        if(dead == 404){
+            processRunning--;
+        }
         //Seperate the message by white space and assign it ot page number, memory address, and read/write
         procChoice = strtok(buf.strData, " ");
         seperate = 0;
