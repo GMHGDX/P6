885f864edb979d1da0862b2a96437f01025970d7
GabrielleHieken <gabriellehieken@gmail.com>
2023-05-12 20:09:15 -0500

    print frame table

--- user_proc.c
+++ user_proc.c
@@ -95,7 +95,7 @@ int main(int argc, char *argv[]){
             buf.intData = getpid();
             buf.mtype = (long)getppid();
             if(msgsnd(msqid, &buf, sizeof(msgbuffer), 0 == -1)){ perror("W2 msgsnd from child to parent failed\n"); exit(1); }
-            wait(1);
+            sleep(1);
             if (msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) { perror("2 failed to receive message from parent\n"); exit(1); }
             frameNumber = atoi(buf.strData);
 
